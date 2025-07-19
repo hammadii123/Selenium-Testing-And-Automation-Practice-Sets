a95a2a342f29c76c194c7173b6274b451c91c43c
Hammad2 <hammadworks123@gmail.com>
2025-07-19 14:13:50 +0500

    form page class

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+from selenium import webdriver
+import time
 
 class DragPage:
     def _init_(self,driver):
@@ -14,4 +16,51 @@ class DragPage:
         target=self.driver.find_element(*self.drag_target)
 
         actions=ActionChains(self.driver)
-        ActionChains.drag_and_drop(source,target).perform()
+        actions.drag_and_drop(source,target).perform()
+# test scrpit
+
+driver=webdriver.Chrome()
+driver.get("https://www.example.com")
+drag_test=DragPage(driver)
+
+print("Testing start hogyi he")
+drag_test.perform_drag_and_drop()
+
+
+time.sleep(2)
+
+
+class FormPage:
+    def __init__(self,driver):
+        self.driver=driver
+
+        self.first_name=(By.ID,"firstName")
+        self.last_name=(By.ID,"lastName")
+        self.phone=(By.ID,"Phone")
+        self.email=(By.ID,"Email")
+        self.password=(By.ID,"Password")
+        self.country=(By.ID,"Country")
+        self.state=(By.ID,"State")
+        self.city=(By.ID,"City")
+        self.fax=(By.ID,"Fax")
+        self.postal_code=(By.ID,"Postal_code")
+        self.submitBtn=(By.ID,"submit_btn")
+
+    def fill_form(self,data):
+        self.driver.find(*self.first_name).send_keys(data["first_name"])
+        self.driver.find(*self.lastName).send_keys(data["lastName"])
+        self.driver.find(*self.phone).send_keys(data["Phone"])
+        self.driver.find(*self.email).send_keys(data["Email"])
+        self.driver.find(*self.password).send_keys(data["Password"])
+        self.driver.find(*self.country).send_keys(data["Country"])
+        self.driver.find(*self.state).send_keys(data["State"])
+        self.driver.find(*self.city).send_keys(data["City"])
+        self.driver.find(*self.fax).send_keys(data["Fax"])
+        self.driver.find(*self.postal_code).send_keys(data["Postal_code"])
+
+    def submitForm(self):
+        self.driver.find_element(*self.submitBtn).click()
+
+
+
+        
